03ab959e4521851398309e9601b52dda82a6a102
Spectrum-hue <66886619+Spectrum-hue@users.noreply.github.com>
2021-09-06 20:35:30 +0200

    Update Favourites.java

--- Favourites.java
+++ Favourites.java
@@ -1,5 +1,27 @@
 package com.thisisspringboot.model;
 
-public class Favourites {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="favourites")
+public class Favourites {
+	//@Id
+	//private Long id;
+    @Id
+	private String symbol;
+    
+	private String exchange;
+	private String datetime;
+	private String open;
+	private String high;
+	private String low;
+	private String close;
+	
 }
