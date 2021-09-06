aeff3ffc92c926bfafe1877b761d30349a0389c3
Spectrum-hue <66886619+Spectrum-hue@users.noreply.github.com>
2021-09-06 20:37:42 +0200

    Update FavouriteRepository.java

--- FavouriteRepository.java
+++ FavouriteRepository.java
@@ -1,3 +1,12 @@
 package com.thisisspringboot.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.thisisspringboot.model.Favourites;
+
+public interface FavouriteRepository extends JpaRepository<Favourites, Integer> {
+
+	Favourites save(Favourites favourites);
+	
+}
+	
