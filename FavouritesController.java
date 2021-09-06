04256fb9bd2c34b9bcd6e30507ec78a2a92561ad
Spectrum-hue <66886619+Spectrum-hue@users.noreply.github.com>
2021-09-06 20:34:29 +0200

    Update FavouritesController.java

--- FavouritesController.java
+++ FavouritesController.java
@@ -1,5 +1,36 @@
 package com.thisisspringboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thisisspringboot.model.Favourites;
+import com.thisisspringboot.repository.FavouriteRepository;
+
+@RestController
 public class FavouritesController {
 
+    @Autowired
+    private FavouriteRepository repository;
+
+    @RequestMapping(value="/favourites",method=RequestMethod.GET)
+    public List<Favourites> getFavourites(){
+
+        return repository.findAll();
+    }
+
+    @RequestMapping(value="/favourites/",method=RequestMethod.POST)
+    public Favourites createFavourites(@RequestBody Favourites favourites) {
+
+        return repository.save(favourites);
+    }
+
+
+    
 }
+
+
